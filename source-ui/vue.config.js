3a95becccfcef849424b842b4e9cc30192b93000
陈兴 <2938244975@qq.com>
2024-06-10 16:52:41 +0800

    更新 vue.config.js

--- source-ui/vue.config.js
+++ source-ui/vue.config.js
@@ -56,15 +56,6 @@ module.exports = {
           "^/mp-api": "",
         },
       },
-        '/api': {
-          target: 'http://localhost:3000', // 注意！此处为后端提供的真实接口
-          changeOrigin: true, // 允许跨域
-          pathRewrite: {
-            // 如果接口中是没有api的，那就直接置空，'^/api': ''，
-            // 如果接口中有api，那就得写成{'^/api':'/api'}
-            '^/api': ''
-          }
-        }
     },
     disableHostCheck: true,
   },
